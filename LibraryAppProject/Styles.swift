f660e1f2b4b051ddbbbc63533802ee9f86814d00
MaksTulenchik <TulenchikPlay@yandex.ru>
2025-03-09 19:42:46 +0300

    Initializing the repository for myApp

---
+++ LibraryAppProject/Styles.swift
@@ -0,0 +1,36 @@
+//
+//  Colors.swift
+//  LibraryProjectTesting
+//
+//  Created by Максим Стецук on 04.11.2024.
+//
+
+import SwiftUI
+
+var customFont: Font {
+    return .custom("Helvetica Neue", size: 16)
+}
+
+var serifShapedFont: Font {
+    return .custom("Times New Roman", size: 20)
+}
+
+var subsectionFont: Font {
+    return .custom("Helvetica Neue", size: 20)
+}
+
+var customColor: Color {
+    return Color(UIColor(red: 106 / 255, green: 48 / 255, blue: 72 / 255, alpha: 1))
+}
+
+var customColor2: Color {
+    return Color(UIColor(red: 106 / 255, green: 48 / 255, blue: 72 / 255, alpha: 0.1))
+}
+
+var customColor3: Color {
+    return Color(UIColor(red: 106 / 255, green: 48 / 255, blue: 72 / 255, alpha: 0.05))
+}
+
+var customColor4: Color {
+    return Color(UIColor(red: 106 / 255, green: 48 / 255, blue: 72 / 255, alpha: 0.9))
+}
