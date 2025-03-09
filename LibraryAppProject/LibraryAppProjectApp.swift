766b237b9965fed34ec40bf19fcb4ef7adf86895
MaksTulenchik <TulenchikPlay@yandex.ru>
2025-03-09 19:33:38 +0300

    Initial Commit

---
+++ LibraryAppProject/LibraryAppProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryAppProjectApp.swift
+//  LibraryAppProject
+//
+//  Created by Максим Стецук on 09.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct LibraryAppProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
