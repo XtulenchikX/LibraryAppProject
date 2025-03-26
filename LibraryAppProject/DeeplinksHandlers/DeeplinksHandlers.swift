5ad24ff238f7f35120906605f696475f07cc9c0e
MaksTulenchik <TulenchikPlay@yandex.ru>
2025-03-27 01:25:17 +0300

    Add libEvent deepLink handler

--- LibraryAppProject/DeeplinksHandlers/DeeplinksHandlers.swift
+++ LibraryAppProject/DeeplinksHandlers/DeeplinksHandlers.swift
@@ -17,3 +17,17 @@ struct DeepLinkHandler {
         return nil
     }
 }
+
+struct LibEventDeepLinkHandler {
+    static func handleEventDeepLink(url: URL) -> Int? {
+        guard url.scheme == "cbs-pr", url.host == "events" else { return nil }
+
+        let components = url.pathComponents.filter { !$0.isEmpty }
+        
+        if components.count > 1, let eventId = Int(components[1]) {
+            return eventId
+        }
+        
+        return nil
+    }
+}
